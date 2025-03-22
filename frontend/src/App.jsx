e80adcf7a6f88fa5ef8309212352c3952a9182e0
Dino Muslic <dino.muslic51@gmail.com>
2025-03-22 23:14:29 +0100

    Fetched data from the server and displayed it on the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-import "./App.css"
+import ListGroup from "react-bootstrap/ListGroup";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+import "./App.css";
 
 const App = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    // Fetch data from Express.js server
-    axios.get('http://localhost:5000/api/data')
-      .then(response => {
+    axios
+      .get("http://localhost:3000/api/users/")
+      .then((response) => {
+        console.log(response.data);
         setData(response.data);
       })
-      .catch(error => {
-        console.error('There was an error fetching the data!', error);
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
       });
   }, []);
 
   return (
     <div>
-      <h1>Data from Server:</h1>
-      {data ? <p>{data.message}</p> : <p>Loading...</p>}
+      <h1>Users:</h1>
+      {data ? (
+        <ul>
+          {data.map(user => (
+            <li key={user.id}>
+              {user.first_name} {user.last_name}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
-}
+};
 
-export default App
+export default App;
