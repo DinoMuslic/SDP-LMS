87e9a3aeba296aebf8a607329e938e348b168e09
Dino Muslic <dino.muslic51@gmail.com>
2025-03-22 22:38:07 +0100

    App.jsx modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import "./App.css"
 
 const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from Express.js server
+    axios.get('http://localhost:5000/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
 
   return (
-    <>
-      <div>Hello :D</div>
-    </>
-  )
+    <div>
+      <h1>Data from Server:</h1>
+      {data ? <p>{data.message}</p> : <p>Loading...</p>}
+    </div>
+  );
 }
 
 export default App
