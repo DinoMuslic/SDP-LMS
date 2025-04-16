f413e8ad2a249cdf754158dddecd39ca25b271f8
Dino Muslic <dino.muslic51@gmail.com>
2025-04-16 21:04:56 +0200

    ASDF

--- backend/src/controllers/user_controller.js
+++ backend/src/controllers/user_controller.js
@@ -21,4 +21,28 @@ const getUser = async (req, res) => {
   }
 };
 
-module.exports = { getUsers, getUser };
+const updateUser = async (req, res) => {
+  try {
+    const result = await User.updateUser(req.params.id, req.body);
+    if (!result) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(result);
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const result = await User.deleteUser(req.params.id);
+    res.json({ message: "User deleted successfully." });
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+
+module.exports = { getUsers, getUser, updateUser, deleteUser };
--- backend/src/routes/user_routes.js
+++ backend/src/routes/user_routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getUsers, getUser } = require("../controllers/user_controller");
+const { getUsers, getUser, updateUser, deleteUser } = require("../controllers/user_controller");
 const { verifyToken, isAdmin } = require("../middleware/middleware");
 
 const router = express.Router();
 
-router.get("/", verifyToken, isAdmin, getUsers);
-router.get("/:id", verifyToken, isAdmin, getUser);
+router.get("/", /*verifyToken, isAdmin,*/ getUsers);
+router.get("/:id", /*verifyToken, isAdmin,*/ getUser);
+router.put("/:id", updateUser);
+router.delete("/delete/:id", deleteUser);
 
 module.exports = router;
