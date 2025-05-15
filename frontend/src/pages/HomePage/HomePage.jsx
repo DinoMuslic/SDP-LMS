6b4b97f050347e41a4e08feef566ebfc281fec57
Dino Muslic <dino.muslic51@gmail.com>
2025-05-15 13:41:10 +0200

    Button and input to check book availability

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import MyToast from "../../components/Toast/Toast";
+import BookService from "../../services/book_service";
+
 const HomePage = () => {
+  const [title, setTitle] = useState("");
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+
+  const handleBtnClick = async () => {
+    setShowToast(false);
+    const message = await BookService.isAvailable(title);
+    console.log(message);
+    setToastMessage(message);
+    setShowToast(true);
+  };
+
   return (
     <>
-      <div className="font-xxxl my-center-container">Home Page</div>
+      <div className="my-center-container">
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button onClick={handleBtnClick}>Send</button>
+        { showToast ? <MyToast message={toastMessage} type="info" /> : null }
+      </div>
     </>
   );
 };
