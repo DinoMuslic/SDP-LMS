f8b183a367d9e16bc5ee2b6a82d73f7bb268bd13
Dino Muslic <dino.muslic51@gmail.com>
2025-06-23 18:21:46 +0200

    date & time helper functions

---
+++ backend/src/utils/utils.js
@@ -0,0 +1,25 @@
+const addDays = (dateString, days) => {
+  const date = new Date(dateString);
+  date.setDate(date.getDate() + days);
+  return date;
+};
+
+const getCurrentDateTime = () => {
+  const now = new Date();
+
+  const pad = n => n.toString().padStart(2, '0');
+
+  const year = now.getFullYear();
+  const month = pad(now.getMonth() + 1);
+  const day = pad(now.getDate());
+  const hours = pad(now.getHours());
+  const minutes = pad(now.getMinutes());
+  const seconds = pad(now.getSeconds());
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+// console.log(addDays("2025-07-06", 30));
+console.log(getCurrentDateTime());
+
+module.exports = { addDays, getCurrentDateTime };
