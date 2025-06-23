14b913982a4602c85317d979524e1933de204d89
Dino Muslic <dino.muslic51@gmail.com>
2025-06-23 22:12:12 +0200

    typo fixes and added uodating book amount

--- backend/src/models/book_model.js
+++ backend/src/models/book_model.js
@@ -10,9 +10,9 @@ const getAllBooks = async () => {
   }
 };
 
-const getBookById = async id => {
+const getBookByIsbn = async (id) => {
   try {
-    const rows = await db.query("SELECT * FROM books WHERE id = ?", [id]);
+    const rows = await db.query("SELECT * FROM books WHERE isbn = ?", [id]);
     return rows[0];
   } catch (error) {
     console.error("Database error:", error);
@@ -33,36 +33,53 @@ const addBook = async (
       "INSERT INTO books (isbn, name, genre, year_of_publication, publisher_id, author_id) VALUES (?, ?, ?, ?, ?, ?)",
       [isbn, name, genre, year_of_publication, publisher_id, author_id]
     );
-    return { message: "Book added sucessfully" }
+    return { message: "Book added sucessfully" };
   } catch (error) {
     console.log("Database error:", error);
     throw error;
   }
 };
 
-const updateBook = async(id, { isbn, name, genre, year_of_publication, publisher_id, author_id }) => {
+const updateBook = async ({
+  isbn,
+  name,
+  genre,
+  year_of_publication,
+  publisher_id,
+  author_id,
+}) => {
   try {
     await db.query(
-      "UPDATE books SET isbn = ?, name = ?, genre = ?, year_of_publication = ?, publisher_id = ?, author_id = ? WHERE id = ?",
-      [isbn, name, genre, year_of_publication, publisher_id, author_id, id]
+      "UPDATE books SET name = ?, genre = ?, year_of_publication = ?, publisher_id = ?, author_id = ? WHERE isbn = ?",
+      [isbn, name, genre, year_of_publication, publisher_id, author_id]
     );
   } catch (error) {
     console.log("Database error:", error);
     throw error;
   }
-}
+};
 
-const deleteBook = async id => {
+const updateBookAmount = async (isbn, amount) => {
   try {
-    await db.query(
-      "DELETE FROM books WHERE id = ?", [id]
-    );
+    await db.query("UPDATE books SET amount = ? WHERE isbn = ?", [
+      amount,
+      isbn,
+    ]);
+  } catch (error) {
+    console.log("Database error:", error);
+    throw error;
+  }
+};
+
+const deleteBook = async (id) => {
+  try {
+    await db.query("DELETE FROM books WHERE id = ?", [id]);
   } catch (error) {
     console.log("Database error:", error);
   }
-}
+};
 
-const getAllBooksInfo = async() => {
+const getAllBooksInfo = async () => {
   try {
     const rows = await db.query(
       "SELECT b.name as title, b.genre, b.year_of_publication as yob, b.image, b.description, b.amount , CONCAT(a.first_name, ' ', a.last_name) as author, p.name as publisher FROM books b JOIN authors a ON b.author_id = a.id JOIN publishers p ON b.publisher_id = p.id"
@@ -71,9 +88,9 @@ const getAllBooksInfo = async() => {
   } catch (error) {
     console.log("Database error:", error);
   }
-}
+};
 
-const checkAvailability = async bookName => {
+const checkAvailability = async (bookName) => {
   try {
     const rows = await db.query(
       "SELECT SUM(amount) as amount FROM books WHERE name LIKE ?",
@@ -81,8 +98,17 @@ const checkAvailability = async bookName => {
     );
     return rows[0];
   } catch (error) {
-    console.log("Database error:", error)
+    console.log("Database error:", error);
   }
-}
+};
 
-module.exports = { getAllBooks, getBookById, addBook, updateBook, deleteBook, getAllBooksInfo, checkAvailability };
+module.exports = {
+  getAllBooks,
+  getBookByIsbn,
+  addBook,
+  updateBookAmount,
+  updateBook,
+  deleteBook,
+  getAllBooksInfo,
+  checkAvailability,
+};
