22c0fc4273f7c63d26047ba064b405a320a58d05
Dino Muslic <dino.muslic51@gmail.com>
2025-06-24 17:56:12 +0200

    book returning and some additions to book borrowing

--- backend/src/controllers/borrow_controller.js
+++ backend/src/controllers/borrow_controller.js
@@ -2,20 +2,31 @@ const Borrow = require("../models/borrow_model");
 const Book = require("../models/book_model");
 const User = require("../models/user_model");
 const { addDays, getCurrentDateTime } = require("../utils/utils");
+const { json } = require("express");
 
 const addBorrowing = async (req, res) => {
   try {
     const { student_id, isbn } = req.body;
 
     const user = await User.getUserById(student_id);
-    if(user.length === 0) return res.status(404).json({ error: `Student with id ${student_id} doesn't exist` })
+    if (user.length === 0)
+      return res
+        .status(404)
+        .json({ error: `Student with id ${student_id} doesn't exist` });
 
     const book = await Book.getBookByIsbn(isbn);
-    if(book.length === 0) return res.status(404).json({ error: `Book with isbn ${isbn} doesn't exist` })
+    if (book.length === 0)
+      return res
+        .status(404)
+        .json({ error: `Book with isbn ${isbn} doesn't exist` });
 
     const isAvailable = await Book.checkAvailability(book[0].name);
-    
-    if(isAvailable[0].amount == 0) return res.status(500).json({ error: "Book not available" })
+
+    if (isAvailable[0].amount == 0)
+      return res.status(500).json({ error: "Book not available" });
+
+    const borrow = await Borrow.getBorrowing(student_id, isbn);
+    if (borrow.length > 0) return res.status(500).json({ error: `${user[0]["first_name"]} ${user[0]["last_name"]} already has the book ${book[0]["name"]}` });
 
     const borrow_date = getCurrentDateTime();
     const return_date = addDays(borrow_date, 30);
@@ -24,11 +35,45 @@ const addBorrowing = async (req, res) => {
     const newAmount = book[0].amount - 1;
     await Book.updateBookAmount(isbn, newAmount);
 
-    res.json({ message: `Book ${book[0]["name"]} borrowed to user ${user[0]["first_name"]} ${user[0]["last_name"]}` });
+    res.status(201).json({
+      message: `Book ${book[0]["name"]} borrowed to user ${user[0]["first_name"]} ${user[0]["last_name"]}`,
+    });
   } catch (error) {
     console.error("Error creating borrowing:", error);
     res.status(500).json({ error: "Server error" });
   }
 };
 
-module.exports = { addBorrowing }
+const returnBook = async (req, res) => {
+  try {
+    const { student_id, isbn } = req.body;
+
+    const user = await User.getUserById(student_id);
+    if (user.length === 0)
+      return res
+        .status(404)
+        .json({ error: `Student with id ${student_id} doesn't exist` });
+
+    const book = await Book.getBookByIsbn(isbn);
+    if (book.length === 0)
+      return res
+        .status(404)
+        .json({ error: `Book with isbn ${isbn} doesn't exist` });
+    
+    const borrow = await Borrow.getBorrowing(student_id, isbn);
+    if (borrow.length === 0) return res.status(404).json({ error: ` ${user[0]["first_name"]} ${user[0]["last_name"]} didn't borrow the book ${book[0]["name"]}` })
+
+    await Borrow.returnBook(student_id, isbn);
+    const newAmount = book[0].amount + 1;
+    await Book.updateBookAmount(isbn, newAmount);
+
+    return res.status(201).json({
+      message: `Book ${book[0]["name"]} returned by user ${user[0]["first_name"]} ${user[0]["last_name"]}`,
+    });
+  } catch (error) {
+    console.error("Error returning book:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+module.exports = { addBorrowing, returnBook };
