6d012c770f9afd8448742fccbb34f068f7f38a25
Dino Muslic <dino.muslic51@gmail.com>
2025-06-24 17:57:31 +0200

    return book route

--- backend/src/routes/borrowing_routes.js
+++ backend/src/routes/borrowing_routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { addBorrowing } = require("../controllers/borrow_controller");
+const { addBorrowing, returnBook } = require("../controllers/borrow_controller");
 
 
 const router = express.Router();
 
 router.post("/add", addBorrowing);
+router.post("/return", returnBook);
 
 module.exports = router;
