246b703c88cd4755904d45881ac57dc1ea916574
Dino Muslic <dino.muslic51@gmail.com>
2025-06-25 19:08:20 +0200

    user profile page desing

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,44 @@
+import "./ProfilePage.css";
+
 const ProfilePage = () => {
-    return <div className="my-center-container font-xxxl">Profile Page</div>;
-  };
-  
-  export default ProfilePage;
-  
+  return (
+    <div className="profile-center-container">
+      <div className="profile-container">
+        <div className="text-center c-green h1">User Profile</div>
+        <hr /> <br />
+        <div className="h3 mb-4">Full Name: ${}</div>
+        <div className="h3">Email: ${}</div>
+        <br />
+        <hr />
+        <br />
+        <div className="password-fine-container">
+          <div className="left-side">
+            <div className="h3 mb-4">Reset Password</div>
+            <input
+              className="mb-4"
+              type="password"
+              name="password"
+              placeholder="Enter new password"
+            />
+            <br />
+            <input
+              className="mb-4"
+              type="password"
+              name="password"
+              placeholder="Confirm new password"
+            />
+            <br />
+            <button className="my-btn">Change Password</button>
+          </div>
+          <div className="right-side">
+            <div className="h3 mb-4">Total Fines</div>
+            <div className="h4 fine">0 KM</div>
+          </div>
+          <div></div> <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
