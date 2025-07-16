f92ce8cdd93ef9650fd6d7136bca352924c1d943
Dino Muslic <dino.muslic51@gmail.com>
2025-07-16 17:00:28 +0200

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,8 @@ const borrowingRoutes = require("./routes/borrowing_routes");
 const publisherRoutes = require("./routes/publisher_routes");
 
 app.use(cors({
-    origin: "https://sdp-lms-x5tpn.ondigitalocean.app/"
+    origin: "https://sdp-lms-x5tpn.ondigitalocean.app/",ž,
+    credentials: true,
 }));
 app.use(express.json());
 
