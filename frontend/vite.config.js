3584132953d09bd5de40486c8c65fc86134ee504
Dino Muslic <dino.muslic51@gmail.com>
2025-07-16 16:38:41 +0200

    Fix: set base path for DigitalOcean static site

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sdp-lms-frontend/',
   plugins: [react()],
   resolve: {
     alias: {
