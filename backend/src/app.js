243b56651efd4b93e21496504406fa0743d04cb0
Dino Muslic <dino.muslic51@gmail.com>
2025-07-16 19:09:56 +0200

    fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
 const cors = require("cors");
 
@@ -12,17 +12,13 @@ const librarianRoutes = require("./routes/librarian_routes");
 const borrowingRoutes = require("./routes/borrowing_routes");
 const publisherRoutes = require("./routes/publisher_routes");
 
-app.use(cors({
+app.use(
+  cors({
     origin: "https://sdp-lms-x5tpn.ondigitalocean.app",
     credentials: true,
-}));
+  })
+);
 app.use(express.json());
-
-app.use('/sdp-lms-frontend', express.static(path.join(__dirname, '../../frontend/dist')));
-
-app.get('/sdp-lms-frontend/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/authors", authorRoutes);
@@ -32,5 +28,15 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/librarian", librarianRoutes);
 app.use("/api/borrowings", borrowingRoutes);
 app.use("/api/publishers", publisherRoutes);
+
+app.use(
+  "/sdp-lms-frontend",
+  express.static(path.join(__dirname, "../../frontend/dist"))
+);
+
+app.get("/sdp-lms-frontend/*", (req, res) => {
+  console.log("💡 Fallback reached:", req.path);
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 module.exports = app;
